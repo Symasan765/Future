930d83c0700eae9a732780e9f207e5f72aadffde
Omega2948 <yes.kestrel@gmail.com>
2018-09-10 11:30:47 +0900

    NavMeshに従ってボスが目標地点へ移動するスクリプトを作成

---
+++ Assets/Script/Tamura/NavMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class NavMove : MonoBehaviour {
+    public Transform target;
+    NavMeshAgent agent;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        agent.SetDestination(target.position);
+    }
+}
