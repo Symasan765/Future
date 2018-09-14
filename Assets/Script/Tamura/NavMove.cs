157779fb57fa5c9b4f462f3a69f515d1534255aa
Omega2948 <yes.kestrel@gmail.com>
2018-09-14 09:14:47 +0900

    ボスの近距離攻撃を追加

--- Assets/Script/Tamura/NavMove.cs
+++ Assets/Script/Tamura/NavMove.cs
@@ -6,11 +6,39 @@ public class NavMove : MonoBehaviour {
     public Transform target;
     NavMeshAgent agent;
 
+    public Vector3 warpPoint;
+
+    Material mat;
+
+    [SerializeField]
+    GameObject shouko;
+
     void Start() {
         agent = GetComponent<NavMeshAgent>();
+        
+        //agent.Warp(warpPoint);
+
+        mat = GetComponent<Renderer>().material;
+        Debug.Log(mat);
     }
 
     void Update() {
         agent.SetDestination(target.position);
+
+        //agent.Warp(warpPoint);
+
+        Attack();
+    }
+
+    void Attack() {
+        if (agent.remainingDistance <= 0.1) {
+            Debug.LogError("Attacked");
+
+            mat.color = Color.red;
+            Instantiate(shouko, this.transform.position, this.transform.rotation);
+        }
+        else {
+            mat.color = Color.white;
+        }
     }
 }
