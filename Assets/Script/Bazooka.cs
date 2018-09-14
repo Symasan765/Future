c9e7acf915b62acc2729de1c046fb0ed92d7e2e3
WIN10OHS50261\LEESHINHEE <wmfehdgml@gmail.com>
2018-09-14 10:00:59 +0900

    バズーカーの投げる機能追加

--- Assets/Script/Bazooka.cs
+++ Assets/Script/Bazooka.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class Bazooka : MonoBehaviour {
     [SerializeField]
+    private GameObject BazookaPrefab;
     private GameObject BazookaObject;
     private int SyoukouNum;
-    
+    private int BazookaMaxMum = 20;
+
+    private Vector3 TargetPos;
     
     //バズーカーのオブジェクトをPrefabからロードしておく。
 	void Start () 
     {
-        BazookaObject = Resources.Load("Prefab/Lee/Bazooka") as GameObject;
+        BazookaPrefab = Resources.Load("Prefab/Lee/Bazooka") as GameObject;
     }
 
     //Update関数の中で、特定の条件になるとバズーカーオブジェクトを生成する関数を呼び出す。
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            ShotBazooka(new Vector3(0.0f, 10.0f, 5.0f));
-        }
+        ShotBazooka();
     }
-
     //バズーカーを生成する。動的でオブジェクトを生成。in_posの位置に移動させる。
-    void ShotBazooka(Vector3 in_pos)
+    void ShotBazooka()
     {
-        GameObject BazookaBullet = Instantiate(BazookaObject);
-        BazookaBullet.transform.position = in_pos;
+        Vector3 TargetVector = BazookaObject.transform.position - TargetPos;
+        Vector3.Normalize(TargetVector);
+        BazookaObject.transform.position += TargetVector;
     }
 
+    void SetBazooka(Vector3 in_TargetPos)
+    {
+        TargetPos = in_TargetPos;
+    }
     //証拠の数を調べる。現状では証拠タグをつけたオブジェクトの数を数えている。
     void FindSyoukou()
     {
