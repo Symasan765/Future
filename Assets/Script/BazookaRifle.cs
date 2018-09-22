cc1b3c639104221fb0b27397726f52d72f3ab00f
LEE@Desktop <wmfehdgml@gmail.com>
2018-09-22 16:54:07 +0900

    担当分の作業完成

--- Assets/Script/BazookaRifle.cs
+++ Assets/Script/BazookaRifle.cs
@@ -2,15 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class B : MonoBehaviour {
+/// <summary>
+/// バズーカーの砲台（？）のスクリプト。
+/// バズーカーの発射に関する動作を定義している。
+/// バズーカーのオブジェクトにコンポネントとして当てておく。
+/// </summary>
+public class BazookaRifle : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject BazookaPrefab;               //ここにバズーカーのPrefabをUnityのInspectorで割当しておく。（またはResourece.Loadでロードするといい）
+    private float SyoukoDistance = 3.0f;            //証拠を認識する範囲。
+    private int SyoukoNum = 0;                      //全体の証拠の数。
+    private int NearSyoukoNum = 0;                  //近づいた証拠の数。
+    private GameObject[] Syouko_temp = new GameObject[3];
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update()
+    {
+        GameObject Boss = GameObject.FindGameObjectWithTag("BOSS");
+        Debug.Log(Boss);
+        //証拠を探す。
+        DetectSyouko();
+
+        //証拠が3つ以上だとバズーカーを発射し、証拠を削除。
+        if (NearSyoukoNum == 3)
+        {
+            Instantiate(BazookaPrefab);
+            BazookaPrefab.GetComponent<BazookaBullet>().SetBazooka(Boss.gameObject.transform.position, this.transform.position);
+            for(int i=0;i<3;i++)
+            {
+                Destroy(Syouko_temp[i]);
+            }
+        }
+
+
+    }
+
+    //証拠を探す関数。
+    void DetectSyouko()
+    {
+        //とりあえずSYOUKOタグがついているオブジェクトを全部獲得。
+        GameObject[] Syouko = GameObject.FindGameObjectsWithTag("SYOUKO");
+        SyoukoNum = Syouko.Length;
+        int NearSyoukoNum_Temp = 0;
+
+        //証拠とバズーカーの距離がパラメータより近いと近づいた証拠だと判断する。
+        for (int i = 0; i < SyoukoNum; i++)
+        {
+            if (Vector3.Distance(Syouko[i].gameObject.transform.position, this.gameObject.transform.position) <= SyoukoDistance)
+            {
+                Syouko_temp[NearSyoukoNum_Temp] = Syouko[i];        //あとで削除するために変数でオブジェクトを持っておく。
+                NearSyoukoNum_Temp++;            
+            }
+        }
+        NearSyoukoNum = NearSyoukoNum_Temp;
+    }
+
+
 }
