5168dfba6c975ccce2ef72a76ac4715b8ace3feb
sekine <sekine.soushihal0829@gmail.com>
2018-10-05 02:24:27 +0900

    敵のプレハブを追加

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	[SerializeField]
+	private float DamagePower = 10;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();	
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//プレイヤーの攻撃判定に当たったら吹っ飛ぶ
+		if (other.name == "AttackCollision")
+		{
+			Player player = other.transform.parent.GetComponent<Player>();
+			player.HitAttackCollision();
+			rb.AddForce(other.transform.forward * DamagePower, ForceMode.Impulse);
+		}
+	}
+
+}
