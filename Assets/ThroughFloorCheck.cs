d7c24a9c8af2219d4a1a17c56c9978fe58a161f5
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-10-04 12:36:56 +0900

    すり抜け床の処理を実装した

--- Assets/ThroughFloorCheck.cs
+++ Assets/ThroughFloorCheck.cs
@@ -3,30 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ThroughFloorCheck : MonoBehaviour {
-    CapsuleCollider collider;
 
-	void Awake () {
-        collider = GetComponent<CapsuleCollider>();
+	Rigidbody m_Rigid;
+	CapsuleCollider m_Capsule;
+
+	int ThroughFloorLayerNo = 0;
+	int DefaultLayerNo = 0;
+
+	private void Start()
+	{
+		m_Rigid = GetComponent<Rigidbody>();
+		m_Capsule = GetComponent<CapsuleCollider>();
+		ThroughFloorLayerNo = LayerMask.NameToLayer("ThroughFloor");
+		DefaultLayerNo = LayerMask.NameToLayer("Default");
 	}
 
-    void OnCollisionEnter(Collision other) {
-        if (other.gameObject.CompareTag("ThroughFloor")) {
-            Debug.Log("Enter");
-            collider.isTrigger = true;
-            // この方法だとisTriggerがtrueになった時に他の当たり判定との
-        }
-    }
+	private void Update()
+	{
+		// まずはキャラの足元の座標を計算する
+		Vector3 capsulePos = transform.position + m_Capsule.center;
+		Vector3 footPos = new Vector3(capsulePos.x, capsulePos.y - (m_Capsule.height / 2.0f + 0.001f), capsulePos.z);   // 0.001fは地面から少し上からレイを飛ばすため
 
-    void OnCollisionStay(Collision other) {
-        if (other.gameObject.CompareTag("ThroughFloor")) {
-            Debug.Log("Stay");
-        }
-    }
+		// レイを作成
+		Ray ray = new Ray(footPos, Vector3.up);
 
-    void OnCollisionExit(Collision other) {
-        if (other.gameObject.CompareTag("ThroughFloor")) {
-            Debug.Log("Exit");
-            collider.isTrigger = false;
-        }
-    }
+		// レイヤーの管理番号を取得
+		
+		// マスクへの変換（ビットシフト）
+		int layerMask = 1 << ThroughFloorLayerNo;
+
+		gameObject.layer = DefaultLayerNo;
+
+		RaycastHit hit;
+		// Rayが衝突したかどうか(*1.2fは少し長めに判定を取るため)
+		if (Physics.Raycast(ray, out hit, m_Capsule.height * 1.2f, layerMask))
+		{
+			gameObject.layer = ThroughFloorLayerNo;
+		}
+	}
 }
