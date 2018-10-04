78f57aba63a3534c93aa736bcb3a587ed0889cb3
Omega2948 <yes.kestrel@gmail.com>
2018-10-04 11:41:33 +0900

    床すり抜け用スクリプト(未完成)を追加

---
+++ Assets/ThroughFloorCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThroughFloorCheck : MonoBehaviour {
+    CapsuleCollider collider;
+
+	void Awake () {
+        collider = GetComponent<CapsuleCollider>();
+	}
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("ThroughFloor")) {
+            Debug.Log("Enter");
+            collider.isTrigger = true;
+        }
+    }
+
+    void OnCollisionStay(Collision other) {
+        if (other.gameObject.CompareTag("ThroughFloor")) {
+            Debug.Log("Stay");
+        }
+    }
+
+    void OnCollisionExit(Collision other) {
+        if (other.gameObject.CompareTag("ThroughFloor")) {
+            Debug.Log("Exit");
+            collider.isTrigger = false;
+        }
+    }
+}
