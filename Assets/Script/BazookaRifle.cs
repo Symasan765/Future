ea71d0b0faf28d5c6e30e21b07c804c4246814b6
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-10-11 12:03:30 +0900

    バズーカスクリプトの"Syoko"を"Evidence"に置換

--- Assets/Script/BazookaRifle.cs
+++ Assets/Script/BazookaRifle.cs
@@ -11,28 +11,28 @@ public class BazookaRifle : MonoBehaviour
 {
     [SerializeField]
     private GameObject BazookaPrefab;               //ここにバズーカーのPrefabをUnityのInspectorで割当しておく。（またはResourece.Loadでロードするといい）
-    private float SyoukoDistance = 3.0f;            //証拠を認識する範囲。
-    private int SyoukoNum = 0;                      //全体の証拠の数。
-    private int NearSyoukoNum = 0;                  //近づいた証拠の数。
-    private GameObject[] Syouko_temp = new GameObject[3];
+    private float EvidenceDistance = 3.0f;            //証拠を認識する範囲。
+    private int EvidenceNum = 0;                      //全体の証拠の数。
+    public int NearEvidenceNum = 0;                  //近づいた証拠の数。
+    private GameObject[] Evidence_temp = new GameObject[3];
+	
 
-
-    void Update()
+	void Update()
     {
         GameObject Boss = GameObject.FindGameObjectWithTag("BOSS");
         Debug.Log(Boss);
-        //証拠を探す。
-        DetectSyouko();
+		//証拠を探す。
+		DetectEvidence();
 
         //証拠が3つ以上だとバズーカーを発射し、証拠を削除。
-        if (NearSyoukoNum == 3)
+        if (NearEvidenceNum == 3)
         {
             Instantiate(BazookaPrefab);
             BazookaPrefab.transform.position = this.transform.position;
             BazookaPrefab.GetComponent<BazookaBullet>().SetBazooka(Boss.gameObject.transform.position, this.transform.position);
             for(int i=0;i<3;i++)
             {
-                Destroy(Syouko_temp[i]);
+                Destroy(Evidence_temp[i]);
             }
         }
 
@@ -40,23 +40,23 @@ public class BazookaRifle : MonoBehaviour
     }
 
     //証拠を探す関数。
-    void DetectSyouko()
+    void DetectEvidence()
     {
         //とりあえずSYOUKOタグがついているオブジェクトを全部獲得。
         GameObject[] Syouko = GameObject.FindGameObjectsWithTag("SYOUKO");
-        SyoukoNum = Syouko.Length;
-        int NearSyoukoNum_Temp = 0;
+		EvidenceNum = Syouko.Length;
+        int NearEvidenceNum_Temp = 0;
 
         //証拠とバズーカーの距離がパラメータより近いと近づいた証拠だと判断する。
-        for (int i = 0; i < SyoukoNum; i++)
+        for (int i = 0; i < EvidenceNum; i++)
         {
-            if (Vector3.Distance(Syouko[i].gameObject.transform.position, this.gameObject.transform.position) <= SyoukoDistance)
+            if (Vector3.Distance(Syouko[i].gameObject.transform.position, this.gameObject.transform.position) <= EvidenceDistance)
             {
-                Syouko_temp[NearSyoukoNum_Temp] = Syouko[i];        //あとで削除するために変数でオブジェクトを持っておく。
-                NearSyoukoNum_Temp++;            
+				Evidence_temp[NearEvidenceNum_Temp] = Syouko[i];        //あとで削除するために変数でオブジェクトを持っておく。
+				NearEvidenceNum_Temp++;            
             }
         }
-        NearSyoukoNum = NearSyoukoNum_Temp;
+		NearEvidenceNum = NearEvidenceNum_Temp;
     }
 
 
