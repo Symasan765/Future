86d770ba9bf76ecfc88c98538afcd40460d37a76
WIN10OHS50261\LEESHINHEE <wmfehdgml@gmail.com>
2018-10-12 10:19:30 +0900

    タイトルの基本的な構造（イメージ）作成

--- Assets/Script/TitleScript.cs
+++ Assets/Script/TitleScript.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TitleScript : MonoBehaviour {
+    public Camera CameraObject;
+
+    public Vector3 MovingSpeed;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        CameraObject.transform.position += MovingSpeed;
 	}
 }
