d57b99bb7940b91ca39b87f1bd6fea44e03a6578
WIN10OHS50261\LEESHINHEE <wmfehdgml@gmail.com>
2018-10-15 10:16:11 +0900

    タイトルのイメージのベース制作

--- Assets/Script/TitleScript.cs
+++ Assets/Script/TitleScript.cs
@@ -1,21 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScript : MonoBehaviour {
-    public Camera CameraObject;
+    public Camera CameraObject;     //カメラオブジェクト
 
-    public Vector3 MovingSpeed;
+    public Vector3 MovingSpeed;     //カメラが動く速度。Unityのインスペクターで数値を設定
 
-	// Use this for initialization
+    public enum SceneList
+    {
+        Title = 0,
+        CharactorSelect,
+        GameMain
+    };
+	
 	void Start ()
     {
 		
 	}
 	
-	// Update is called once per frame
+	
 	void Update ()
     {
         CameraObject.transform.position += MovingSpeed;
+        for (int i = 0; i < 4; i++)
+        {
+            if (XPad.Get.GetTrigger(XPad.KeyData.A, i)||Input.GetKeyDown(KeyCode.A))
+            {
+                SceneManager.LoadScene((int)SceneList.CharactorSelect);
+                break;
+            }
+        }
 	}
 }
