d2163ead6673ab4d0b2393c8a08686a935e8ed6b
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-10-15 16:36:07 +0900

    ボスの攻撃範囲用コライダーの実装(途中)

---
+++ Assets/Script/Boss/BossAttackArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// このスクリプトがアタッチされたオブジェクトの範囲をボスの攻撃範囲として別のスクリプトに渡す
+/// </summary>
+public class BossAttackArea : MonoBehaviour {
+
+	public Vector2 GetAreaPos()
+	{
+		return transform.position;
+	}
+
+	public Vector2 GetAreaRange()
+	{
+		return transform.localScale;
+	}
+}
