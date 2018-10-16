becee63c1d7c02dfd077efd9dd0611b6614f85db
LEE@NotePC <wmfehdgml@gmail.com>
2018-10-16 19:37:44 +0900

    フェードの機能追加

---
+++ Assets/Script/Fade.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// フェード用スクリプト。
+/// 使い方：
+/// １．フェードする画像オブジェクトにこのスクリプトをつける。
+/// 注意：かならず画像はImageであること。
+/// ２．フェードを使うシーンのスクリプトで、フェードさせる画像のオブジェクトを確保する。
+/// (FindObjectなどでオブジェクトを探す）
+/// ３．その画像オブジェクト.GetComponent<Image>().SetFadeでフェードをセットアップする。
+/// ４．フェードの完了後、好きな作業をする。
+/// 
+/// </summary>
+public class Fade : MonoBehaviour {
+    public enum FadeOption { FADEIN,FADEOUT};               //フェードの種類を定義
+    GameObject thisObject;                               //このスクリプトがついているゲームオブジェクト
+    Image thisImage;                                     //そのオブジェクトからImageコンポネントを持ってきて確保する用途
+    bool DoFadeIn = false;                                //フェードイン中
+    bool DoFadeOut = false;                               //フェードアウト中
+    bool FadeDone = false;                               //フェードが完了したかどうか
+    bool FadeAll = false;                                 //このオブジェクトが画面全体のフェードのものかどうか
+    Color FadeSpeed = new Color(0.0f, 0.0f, 0.0f, 0.01f);  //フェードする速さ
+
+    Vector3 initPos;
+	void Start ()
+    {
+        thisObject = this.gameObject;                      //ゲームオブジェクト確保
+        thisImage = thisObject.GetComponent<Image>();      //Imageのコンポネント確保
+        initPos = GetComponent<RectTransform>().localScale;
+
+    }
+	
+	void Update ()
+    {
+        GetComponent<RectTransform>().localScale = initPos * ((Mathf.Sin(Time.time) + 1.0f) /2.0f + 0.5f);
+
+        //画面全体のフェードであれば
+        if (FadeAll)
+        {
+            //フェードイン処理
+            if (DoFadeIn == true)
+            {
+                FadeDone = false;
+                thisImage.color -= FadeSpeed;
+                if (thisImage.color.a <= 0.0f)
+                {
+                    FadeDone = true;
+                    DoFadeIn = false;
+                }
+            }
+
+            //フェードアウト処理
+            if (DoFadeOut == true)
+            {
+                FadeDone = false;
+                thisImage.color += FadeSpeed;
+                if (thisImage.color.a >= 1.0f)
+                {
+                    FadeDone = true;
+                    DoFadeOut = false;
+                }
+            }
+        }
+        //画面全体のフェードではなければ
+        else
+        {
+            //フェードイン処理
+            if (DoFadeIn == true)
+            {
+                FadeDone = false;
+                thisImage.color += FadeSpeed;
+                if (thisImage.color.a >= 1.0f)
+                {
+                    FadeDone = true;
+                    DoFadeIn = false;
+                }
+            }
+            //フェードアウト処理
+            if (DoFadeOut == true)
+            {
+                FadeDone = false;
+                thisImage.color -= FadeSpeed;
+                if (thisImage.color.a <= 0.0f)
+                {
+                    FadeDone = true;
+                    DoFadeOut = false;
+                }
+            }
+        }
+		
+    }
+
+    /// <summary>
+    /// フェードの種類を指定してセットする
+    /// </summary>
+    /// <param name="Option">フェードイン・フェードアウト</param>
+    /// <param name="isThisImageAll">画面全体のフェードかどうか</param>
+    public void SetFade(int Option, bool isThisImageAll)
+    {
+        switch (Option)
+        {
+            case (int)FadeOption.FADEIN:
+                DoFadeIn = true;
+                break;
+            case (int)FadeOption.FADEOUT:
+                DoFadeOut = true;
+                break;
+        }
+        if (isThisImageAll == true)
+        {
+            FadeAll = true;
+        }
+    }
+    
+    /// <summary>
+    /// フェードが完了したかどうか
+    /// </summary>
+    /// <returns></returns>
+    public bool IsFadeDone()
+    {
+        return FadeDone;
+    }
+
+    public void SetFadeFlag(bool temp)
+    {
+        FadeDone = temp;
+    }
+}
--- Assets/Script/TitleScript.cs
+++ Assets/Script/TitleScript.cs
@@ -2,24 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScript : MonoBehaviour {
     public Camera CameraObject;     //カメラオブジェクト
 
     public Vector3 MovingSpeed;     //カメラが動く速度。Unityのインスペクターで数値を設定
-
+    public GameObject FadeImage;
+    public GameObject FadeImage2;
     public enum SceneList
     {
         Title = 0,
         CharactorSelect,
         GameMain
     };
-	
-	void Start ()
-    {
-		
-	}
-	
 	
 	void Update ()
     {
@@ -28,9 +24,24 @@ public class TitleScript : MonoBehaviour {
         {
             if (XPad.Get.GetTrigger(XPad.KeyData.A, i)||Input.GetKeyDown(KeyCode.A))
             {
-                SceneManager.LoadScene((int)SceneList.CharactorSelect);
+                FadeImage.GetComponent<Fade>().SetFade((int)Fade.FadeOption.FADEOUT,true);
+               // SceneManager.LoadScene((int)SceneList.CharactorSelect);
                 break;
             }
         }
-	}
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            FadeImage.GetComponent<Fade>().SetFade((int)Fade.FadeOption.FADEIN, true);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            FadeImage2.GetComponent<Fade>().SetFade((int)Fade.FadeOption.FADEOUT, false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            FadeImage2.GetComponent<Fade>().SetFade((int)Fade.FadeOption.FADEIN, false);
+        }
+    }
+
+  
 }
