207ec20e8175a318d8686833c7c5fec89bb6eb24
sekine <sekine.soushihal0829@gmail.com>
2018-10-26 23:35:55 +0900

    バズーカに近づいた時、アイテムを置く&アイテムが吸い込まれる

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -6,13 +6,21 @@ public class Item : MonoBehaviour {
 
 	private float MoveSpeed = 5.0f;
 
+	private GameObject bazookaObj;
+	private BazookaRifle bazookaRifle;
 	private Vector3 getPosition;
 	[HideInInspector]
 	public bool flgMoveToGetPos;
-	
+
+	public bool isScaleDown = false;
+	public bool isHold = false;
+	private BoxCollider boxCollider;
+	private float cntScaleDownTime = 0;
 	void Start ()
 	{
-		
+		boxCollider = GetComponent<BoxCollider>();
+		bazookaObj = GameObject.Find("BazookaRifle");
+		bazookaRifle = bazookaObj.GetComponent<BazookaRifle>();
 	}
 	
 	void Update ()
@@ -25,6 +33,36 @@ public class Item : MonoBehaviour {
 				transform.localPosition += vec * MoveSpeed * Time.deltaTime;
 			}
 		}
+
+		if (isScaleDown)
+		{
+			cntScaleDownTime += Time.deltaTime;
+			if (cntScaleDownTime >= 1.0f)
+			{
+				cntScaleDownTime = 1.0f;
+			}
+			transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0, 0, 0), cntScaleDownTime);
+			if (cntScaleDownTime >= 1)
+			{
+				bazookaRifle.NearEvidenceNum++;
+				Destroy(gameObject);
+			}
+		}
+
+		if (Vector3.Distance(transform.position, bazookaObj.transform.position) <= bazookaRifle.EvidenceDistance && !IsHold())
+		{
+			isHold = true;
+			boxCollider.enabled = false;
+			SetItemLocalPosition(bazookaObj.transform.position);
+			flgMoveToGetPos = true;
+			isScaleDown = true;
+		}
+
+	}
+
+	public bool IsHold()
+	{
+		return isHold;
 	}
 
 	public void SetItemLocalPosition(Vector3 _pos)
