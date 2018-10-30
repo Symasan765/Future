990be770eee093c0f9711de59ad5d51e97db7580
sekine <sekine.soushihal0829@gmail.com>
2018-10-30 11:47:30 +0900

    背景のClousとDotPatternをカメラの移動に応じて若干移動するように変更(いらなかったらけします)

---
+++ Assets/Script/BackGroundMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMove : MonoBehaviour {
+
+	private Vector3 startPosition;
+	private Vector3 startCameraPosition;
+	private GameObject CameraObj;
+	public float MovePosition = 0.5f;
+	public float Speed = 0.5f;
+
+	void Start ()
+	{
+		CameraObj = GameObject.Find("Main Camera");
+		startPosition = transform.position;
+		startCameraPosition = CameraObj.transform.position;
+	}
+	
+	void Update ()
+	{
+		Vector3 cameraMovePos = new Vector3(CameraObj.transform.position.x - startCameraPosition.x, CameraObj.transform.position.y - startCameraPosition.y, CameraObj.transform.position.z);
+
+		transform.position = Vector3.Lerp(transform.position, new Vector3(startPosition.x + cameraMovePos.x * MovePosition, startPosition.y, startPosition.z), Speed);
+	}
+}
