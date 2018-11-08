8391e6da64f3685909879c24ab9612cc61785815
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-11-08 14:16:50 +0900

    タイムライン上でスカイドームの色を変更出来るようにした

--- Assets/Script/Timeline/BloomInit.cs
+++ Assets/Script/Timeline/BloomInit.cs
@@ -6,6 +6,9 @@ using UnityEngine.PostProcessing;
 public class BloomInit : MonoBehaviour {
 	PostProcessingBehaviour behaviour;
 	public float m_InitBloom = 0.0f;
+	public Material SkyMaterial;
+	public float m_SkyValue = 1.98f;
+
 	// Use this for initialization
 	void Start()
 	{
@@ -14,5 +17,10 @@ public class BloomInit : MonoBehaviour {
 		var Settings = behaviour.profile.bloom.settings;
 		Settings.bloom.intensity = m_InitBloom;
 		behaviour.profile.bloom.settings = Settings;
+
+		if(SkyMaterial != null)
+		{
+			SkyMaterial.mainTextureScale = new Vector2(m_SkyValue, 1.0f);
+		}
 	}
 }
