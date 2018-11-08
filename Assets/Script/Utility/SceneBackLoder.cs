06f5d68881d570ed572a434c118217c9df776b4c
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-11-08 18:46:23 +0900

    裏読みスクリプトの実装

---
+++ Assets/Script/Utility/SceneBackLoder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBackLoder : MonoBehaviour {
+
+	public string m_NextSceneName;
+
+	private AsyncOperation async;
+	string m_NowSceneName;
+
+	// Use this for initialization
+	void Start()
+	{
+		m_NowSceneName = SceneManager.GetActiveScene().name;
+
+		// シーンを読み込み始める
+		ScenePreLoad(m_NextSceneName);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	void ScenePreLoad(string changeScn)
+	{
+		async = SceneManager.LoadSceneAsync(changeScn, LoadSceneMode.Additive);
+		async.allowSceneActivation = false;
+	}
+
+	IEnumerator SceneLoad()
+	{
+		SceneManager.UnloadScene(m_NowSceneName);
+		async.allowSceneActivation = true;
+		yield return null;
+	}
+
+	/// <summary>
+	/// シーン読み込みが終わっていればシーンを切り替える
+	/// </summary>
+	public void SceneChange()
+	{
+		
+		StartCoroutine("SceneLoad");
+	}
+}
