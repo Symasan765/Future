de3862eb399063accd22c629135ca683bc59e90d
Omega2948 <yes.kestrel@gmail.com>
2018-11-09 23:11:59 +0900

    キャラセレのBGMとアニメーションの調整

--- Assets/Script/Tamura/Arrow.cs
+++ Assets/Script/Tamura/Arrow.cs
@@ -108,8 +108,8 @@ public class Arrow : MonoBehaviour {
 
     // カーソルのアニメーション(左右)
     void AnimateCursor() {
-        float range = 0.25f;
-        float xSpeed = 0.1f; /*+ Random.Range(-0.1f, 0.1f);*/
+        float range = 0.5f;
+        float xSpeed = 0.135f; /*+ Random.Range(-0.1f, 0.1f);*/
 
         cursorAnimateTransform = new Vector3(Mathf.Sin(animAngle) * range, 0.0f, 0.0f);
 
