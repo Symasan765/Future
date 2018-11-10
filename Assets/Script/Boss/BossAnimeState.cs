ea9ca8c82c94dd992ab3d81050b688996ab86794
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-11-10 14:44:32 +0900

    各種スクリプトにて名前検索していたボスマネージャーをタグ検索に変えて処理の高速化を図った

---
+++ Assets/Script/Boss/BossAnimeState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimeState : MonoBehaviour {
+	Animator m_Animator;
+	BossAttackManager m_BossManager;
+
+	// Use this for initialization
+	void Start () {
+		m_Animator = GetComponent<Animator>();
+		m_BossManager = GameObject.FindGameObjectWithTag("BossManager").GetComponent<BossAttackManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
