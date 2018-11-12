182863d5b98414b84e3c934e9ab2e7eeb4be6b20
Omega2948 <yes.kestrel@gmail.com>
2018-11-12 11:39:18 +0900

    消えていたアニメーション制御スクリプトを復旧

---
+++ Assets/AnimateStep.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateStep : MonoBehaviour {
+    Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    void Step() {
+
+    }
+}
