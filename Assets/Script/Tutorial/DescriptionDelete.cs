835e85a7fef68b839b900529e6c5b0d809b8967f
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-11-18 02:06:39 +0900

    操作ボタン説明用プレハブとそのプレハブ群を消滅させるプレハブを作成

---
+++ Assets/Script/Tutorial/DescriptionDelete.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DescriptionDelete : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider collision)
+	{
+		// 一度触れたらすべての攻撃オブジェクトを消滅させる
+		var objs = GameObject.FindGameObjectsWithTag("Description");
+		for (int i = 0; i < objs.Length; i++)
+		{
+			Destroy(objs[i]);
+		}
+	}
+}
