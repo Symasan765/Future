f78e5f50bf4297708af98b4085dfa9a9909d4723
sekine <sekine.soushihal0829@gmail.com>
2018-11-17 22:30:57 +0900

    Stage0プレハブに小物を追加

---
+++ Assets/Script/Propeller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propeller : MonoBehaviour {
+
+	private float Speed = 200.0f;
+
+	private Vector3 angle;
+	// Use this for initialization
+	void Start ()
+	{
+		angle = transform.eulerAngles;
+		angle.y = Random.Range(0, 360);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		angle.y += Time.deltaTime * Speed;
+		transform.eulerAngles = angle;
+	}
+}
