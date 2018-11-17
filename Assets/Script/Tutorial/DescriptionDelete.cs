d0fb32e984fa16173a9c170cd83225d07749327b
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-11-18 02:21:49 +0900

    説明削除プレハブのメッシュ描画をOFFに

--- Assets/Script/Tutorial/DescriptionDelete.cs
+++ Assets/Script/Tutorial/DescriptionDelete.cs
@@ -6,7 +6,7 @@ public class DescriptionDelete : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		GetComponent<MeshRenderer>().enabled = false;
 	}
 	
 	// Update is called once per frame
