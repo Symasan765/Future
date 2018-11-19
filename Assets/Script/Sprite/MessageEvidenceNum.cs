9259a5b2f01581ed9edaa2608eb46cf934ddc23e
sekine <sekine.soushihal0829@gmail.com>
2018-11-20 00:26:09 +0900

    フィーバータイム時は右上に「キミツを入れまくれ！」を表示するようにした。

---
+++ Assets/Script/Sprite/MessageEvidenceNum.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageEvidenceNum : MonoBehaviour {
+
+	public GameObject NormalTextObj;
+	public GameObject FeverTextObj;
+	private FeverManager feverManager;
+
+	[SerializeField]
+	private Vector3 waitPosition;
+	private Vector3[] startPosition = new Vector3[2];
+	private bool drawMessage = false;
+
+	private float speed = 0.4f;
+
+	void Start ()
+	{
+		startPosition[0] = NormalTextObj.transform.localPosition;
+		startPosition[1] = FeverTextObj.transform.localPosition;
+		feverManager = GameObject.Find("FeverManager").GetComponent<FeverManager>();
+	}
+	
+	
+	void Update ()
+	{
+		if (feverManager.IsFever())
+		{
+			NormalTextObj.transform.localPosition = Vector3.Lerp(NormalTextObj.transform.localPosition, waitPosition, speed);
+			FeverTextObj.transform.localPosition = Vector3.Lerp(FeverTextObj.transform.localPosition, startPosition[1], speed);
+		} else
+		{
+			FeverTextObj.transform.localPosition = Vector3.Lerp(FeverTextObj.transform.localPosition, waitPosition, speed);
+			NormalTextObj.transform.localPosition = Vector3.Lerp(NormalTextObj.transform.localPosition, startPosition[0], speed);
+		}
+	}
+}
