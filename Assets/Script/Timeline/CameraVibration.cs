aefc2a43147f6a380dd6ab1e9682bbbd6682617e
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-11-21 11:16:21 +0900

    演出シーンでの振動が起動毎に変わることによるセリフの描画範囲が不安定になる問題を解消

--- Assets/Script/Timeline/CameraVibration.cs
+++ Assets/Script/Timeline/CameraVibration.cs
@@ -6,17 +6,20 @@ public class CameraVibration : MonoBehaviour {
 	GameObject m_Parent;
 	public float m_MaxRange = 0.01f;
 
+	System.Random cRandom;
+
 	// Use this for initialization
 	void Start () {
+		cRandom = new System.Random(0);
 		m_Parent = GameObject.Find("CameraParent");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 pos = m_Parent.transform.position;
-		pos.x += Random.Range(-m_MaxRange, m_MaxRange);
-		pos.y += Random.Range(-m_MaxRange, m_MaxRange);
-		pos.z += Random.Range(-m_MaxRange, m_MaxRange);
+		pos.x += Random.Range(-(float)cRandom.Next(-10000,10000) / 100000.0f, (float)cRandom.Next(-10000, 10000) / 100000.0f);
+		pos.y += Random.Range(-(float)cRandom.Next(-10000, 10000) / 1000000.0f, (float)cRandom.Next(-10000, 10000) / 100000.0f);
+		pos.z += Random.Range(-(float)cRandom.Next(-10000, 10000) / 100000.0f, (float)cRandom.Next(-10000, 10000) / 100000.0f);
 
 		m_Parent.transform.position = pos;
 	}
