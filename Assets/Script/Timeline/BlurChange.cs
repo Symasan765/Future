0abece9df6ae86d40daf86368860bc792dc24696
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-11-21 11:48:26 +0900

    ボスの落下、咆哮モデルの追加

---
+++ Assets/Script/Timeline/BlurChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class BlurChange : MonoBehaviour {
+
+	public PostProcessingProfile m_Profile;
+	public bool m_VibrationFlag = false;
+	GameObject m_MainCameraObj;
+
+	// Use this for initialization
+	void Start () {
+		m_MainCameraObj = GameObject.FindGameObjectWithTag("Main Camera");
+		m_MainCameraObj.GetComponent<PostProcessingBehaviour>().profile = m_Profile;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO ここで振動処理を行う
+		if (m_VibrationFlag)
+		{
+
+		}
+	}
+}
