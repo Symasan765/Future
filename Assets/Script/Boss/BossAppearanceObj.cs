7743490b6ed3dce231df2442032b5a67143fc9c0
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-11-22 21:08:16 +0900

    自分でも何を修正したのか忘れたけど修正

--- Assets/Script/Boss/BossAppearanceObj.cs
+++ Assets/Script/Boss/BossAppearanceObj.cs
@@ -3,31 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class BossAppearanceObj : MonoBehaviour {
+public class BossAppearanceObj : MonoBehaviour
+{
 
 	GameObject m_BossObj;
 	public PlayableDirector m_Timeline;
 	Vector3 m_InitPos;
 	float m_TimeCnt = 0.0f;
+	BossLight m_Light;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		m_BossObj = GameObject.Find("Boss");
 		m_InitPos = m_BossObj.transform.position;
-		m_BossObj.transform.position = Vector3.up * 100.0f;		// ボスを見つけたらカメラから見えない遥か上空へ
+		m_BossObj.transform.position = Vector3.up * 100.0f;     // ボスを見つけたらカメラから見えない遥か上空へ
+		m_Light = GameObject.FindGameObjectWithTag("BossManager").GetComponent<BossLight>();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		// タイムラインが生きている
-		if(m_Timeline.isActiveAndEnabled == true)
+		if (m_Timeline.isActiveAndEnabled == true)
 		{
-			// 初期位置に戻す
+			m_Light.LightChage(true);
+
+			// 初期位置に戻す(若干時間を上げないと一瞬ボスが見えてしまいそう…)
 			m_BossObj.transform.position = m_InitPos;
-			//m_BossObj.transform.position = m_InitPos;
+
 			// タイムラインが終了した
 			if (m_Timeline.duration < m_TimeCnt)
 			{
+				m_Light.LightChage(false);
 				// 自身を消滅させる
 				Destroy(gameObject);
 				// 攻撃を開始！
