5959071513bdd847be6890de695c98b7ce166d7a
sekine <sekine.soushihal0829@gmail.com>
2018-11-23 23:05:33 +0900

    パーセントの色をスマブラに寄せるという意思を追加

--- Assets/Script/UIPlayerMentalNum.cs
+++ Assets/Script/UIPlayerMentalNum.cs
@@ -5,20 +5,45 @@ using UnityEngine.UI;
 public class UIPlayerMentalNum : MonoBehaviour {
 
 	public int playerIndex = 0;
-
+	[SerializeField]
+	private GameObject UIBackNumObj;
 	private GameObject playerObject;
 	private Player player;
-	private Text text;
+	private Text[] text = new Text[2];
 	void Start ()
 	{
-		text = GetComponent<Text>();
+		text[0] = GetComponent<Text>();
+		text[1] = UIBackNumObj.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 
-		text.text = player.GetMentalGauge().ToString();
+		for (int i = 0; i < 2; i++)
+		{
+			text[i].text = player.GetMentalGauge().ToString();
+		}
+
+		if (player.GetMentalGauge() < 100 / 4)
+		{
+			text[0].color = new Color(1, 0.1f ,0.1f, 1.0f);
+		} else
+		{
+			if (player.GetMentalGauge() < (100 / 2))
+			{
+				text[0].color = new Color(0.8f, 0.1f, 0.1f, 1.0f);
+			} else
+			{
+				if (player.GetMentalGauge() < (100 / 4) * 3)
+				{
+					text[0].color = new Color(0.6f, 0.1f, 0.1f, 1.0f);
+				} else
+				{
+					text[0].color = new Color(0.4f, 0.1f, 0.1f, 1.0f);
+				}
+			}
+		}
 
 	}
 
