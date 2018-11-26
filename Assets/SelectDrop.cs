d0013c3c35050b35dc772abcce4f938b6bdad452
Omega2948 <yes.kestrel@gmail.com>
2018-11-26 14:26:11 +0900

    先のコミットの追加し忘れ

---
+++ Assets/SelectDrop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDrop : MonoBehaviour {
+    [SerializeField]
+    float dropTime;
+
+    [SerializeField]
+    Vector3 DropStartPos;
+    [SerializeField]
+    Vector3 DropEndPos;
+
+    [SerializeField]
+    Arrow arrow;
+
+    float nowDropTime;
+
+    void Start() {
+        nowDropTime = 0.0f;
+    }
+
+	void FixedUpdate () {
+        if (arrow.GetCanInput()) {
+            this.transform.position = Vector3.Lerp(DropStartPos, DropEndPos, nowDropTime / dropTime);
+            nowDropTime += Time.fixedDeltaTime;
+        }
+	}
+}
